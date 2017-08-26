3bc083711fbe2a808165050b3e4b0e6bb4d781ae
Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>
2017-08-26 17:35:01 -0300

    Feita a matriz da Glider Gun. OBS.: Ao contrario das outras, é uma matriz 40x40

---
+++ gliderGun.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include "gliderGun.hpp"
+
+using namespace std;
+
+GliderGun::GliderGun(){
+
+    setCell('o',1, 25);
+    setCell('o',2, 23);
+    setCell('o',2, 25);
+    setCell('o',3, 13);
+    setCell('o',3, 14);
+    setCell('o',3, 21);
+    setCell('o',3, 22);
+    setCell('o',3, 35);
+    setCell('o',3, 36);
+    setCell('o',4, 12);
+    setCell('o',4, 16);
+    setCell('o',4, 21);
+    setCell('o',4, 22);
+    setCell('o',4, 35);
+    setCell('o',4, 36);
+    setCell('o',5, 1);
+    setCell('o',5, 2);
+    setCell('o',5, 11);
+    setCell('o',5, 17);
+    setCell('o',5, 21);
+    setCell('o',5, 22);
+    setCell('o',6, 1);
+    setCell('o',6, 2);
+    setCell('o',6, 11);
+    setCell('o',6, 15);
+    setCell('o',6, 17);
+    setCell('o',6, 18);
+    setCell('o',6, 23);
+    setCell('o',6, 25);
+    setCell('o',7, 11);
+    setCell('o',7, 17);
+    setCell('o',7, 25);
+    setCell('o',8, 12);
+    setCell('o',8, 16);
+    setCell('o',9, 13);
+    setCell('o',9, 14);
+
+    setNome("Glider Gun");
+    setGeracoes(150);
+    setTamanho(40);
+
+}
+
+GliderGun::~GliderGun(){}
---
+++ gliderGun.hpp
@@ -0,0 +1,13 @@
+#include "matriz.hpp"
+#ifndef GLIDERGUN_HPP
+#define GLIDERGUN_HPP
+
+using namespace std;
+
+class GliderGun : public Matriz{
+    public:
+        GliderGun();
+        ~GliderGun();
+};
+
+#endif
